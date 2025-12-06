f58d62d8980e79cbfe9fc1c9538c84cebb32f998
ethanwithe <fcezaromeroy@gmail.com>
2025-12-06 01:14:05 -0500

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
+-- Limpia las tablas en orden correcto (por claves foráneas)
+DELETE FROM students_courses;
+DELETE FROM students;
+DELETE FROM courses;
+
+-- Reinicia los IDs
+ALTER TABLE students AUTO_INCREMENT = 1;
+ALTER TABLE courses AUTO_INCREMENT = 1;
+
 -- Inserciones
 INSERT INTO courses (name, modules, credits, fee) VALUES
 ('Matematicas Avanzadas', 10, 5.0, 450.00),
